9abac184466528ecfcf633975a741f01c6d4d20a
Vitor Barbeiro <vitor.agb@hotmail.com>
2023-03-03 11:23:39 -0300

    Created the delete function

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -4,13 +4,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
-  styleUrls: ['./todo-list.component.scss']
+  styleUrls: ['./todo-list.component.scss'],
 })
 export class TodoListComponent {
-
   public taskList: Array<TaskList> = [
-    {task: "Minha nova Task", checked: true},
-    {task: "Minha nova Task 2", checked: false}
+    { task: 'Minha nova Task', checked: true },
+    { task: 'Minha nova Task 2', checked: false },
   ];
 
+  public deleteItemTaskList(event: number) {
+    this.taskList.splice(event, 1);
+  }
+
+  public deleteAllTaskList() {
+    const confirm = window.confirm(
+      'Are you sure you want to delete everything?'
+    );
+    if (confirm) {
+      this.taskList = [];
+    }
+  }
 }
